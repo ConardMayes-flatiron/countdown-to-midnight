7f88b69c8a36a74378d0ec29f6e3164bee413cf6
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-11 23:42:19 -0600

    commit tree e2bb07026610175b865e28df17fdb381aa515b47

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+number=0
+def countdown(number)
+    while number > 0
+        print "#{number} SECOND(S)!\n"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(a)
+    while a > 0
+        print "#{a} SECOND(S)!"
+        sleep(1)
+        a -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
